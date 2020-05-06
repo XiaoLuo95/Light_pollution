ac4f4fcbd7c13ec74b52002be43ec4393dfd400c
XiaoLuo95 <xiao.luo@alumnos.upm.es>
2020-05-06 17:28:48 +0200

    arguments added

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 Para el correcto funcionamiento del script, hacen falta tener instalados los paquetes siguientes:
 
-* requests
-* time
-* numpy
 * re
 * sys
 * math
+* time
+* numpy
 * pandas
+* argparse
+* requests
 * collections
 * Geopy.distance
 
@@ -20,13 +21,30 @@ Situado en el fichero, para ejecutar el script:
 
 ```python
 >>> import subprocess
->>> subprocess.run('python3 light_pollution.py {data file}', shell=True)
+>>> subprocess.run('python3 light_pollution.py {data} {threshold} {OPTIONAL opening {angle_lower angle_upper}} {OPTIONAL distance}', shell=True)
 ```
 
 ### Desde Terminal
 
 ```
-python3 light_pollution.py {data file}
+python3 light_pollution.py {data} {threshold} {OPTIONAL opening {angle_lower angle_upper}} {OPTIONAL distance}
+```
+
+### Help
+```
+usage: light_pollution.py [-h] [-o OPENING OPENING] [-d DISTANCE]
+                          data threshold
+positional arguments:
+  data                  file containing measurement data
+  threshold             percentage of magnitude from minimum under
+                        consideration [0.00-1.00]
+optional arguments:
+  -h, --help            show this help message and exit
+  -o OPENING OPENING, --opening OPENING OPENING
+                        angle opening's lower and upper bound, separated by
+                        whitespace [0-360]
+  -d DISTANCE, --distance DISTANCE
+                        radius in km within to search, default 200
 ```
 
 #### Como fuentes de contaminación lumínica se tienen en cuenta:
--- light_pollution.py
+++ light_pollution.py
@@ -2,11 +2,25 @@ import requests
 import time
 import numpy as np
 import re
-import sys
+import argparse
 import math
 import pandas as pd
 from collections import OrderedDict
 import geopy.distance as dist
+
+# command line arguments parser
+parser = argparse.ArgumentParser()
+parser.add_argument("data", help = "file containing measurement data")
+parser.add_argument("threshold", type=float, help = "percentage of magnitude from minimum under consideration [0.00-1.00]")
+parser.add_argument("-o", "--opening", type=float, nargs=2, action="store", help="angle opening's lower and upper bound, separated by whitespace [0-360]")
+parser.add_argument("-d", "--distance", type=float, action="store", help="radius in km within to search, default 200")
+args = parser.parse_args()
+
+# global distance
+if args.distance is not None:
+    distance = args.distance
+else:
+    distance = 200
 
 # global dataframe for full list of light pollution sources
 df = pd.DataFrame()
@@ -19,12 +33,10 @@ lon = 0
 angle_min = 0
 angle_max = 0
 
-# global distance
-distance = 200
 
 def read_file():
     # Encoding: Western Europe
-    myFile = open(sys.argv[1], "r", encoding='iso8859_15')
+    myFile = open(args.data, "r", encoding='iso8859_15')
     contents = myFile.read()
 
     # Load latitude and longitude
@@ -50,47 +62,52 @@ def read_file():
     total_range = (total.max() - total.min()).round(2)
 
     # Threshold, set as 30% of the range from minimum
-    th = m20.min() + 0.3 * total_range
+    th = m20.min() + float(args.threshold) * total_range
 
-    # Angles at left and right position of the minimum value
     global angle_max, angle_min
-    angle_left = m20[int(np.where(m20 == m20.min())[0] - 1) % 12]
-    angle_right = m20[int(np.where(m20 == m20.min())[0] + 1) % 12]
-
-    # Set default minimum angle opening
-    # If both values at left and right are equal, the opening will be 60º, otherwise 30º
-    if angle_left < angle_right:
-        angle_min = angle_left
-        angle_max = m20.min()
-    elif angle_right < angle_left:
-        angle_min = m20.min()
-        angle_max = angle_right
+    # If optional opening is set with values, just set the global, otherwise proceed to calculate them
+    if args.opening is not None:
+        angle_min = args.opening[0]
+        angle_max = args.opening[1]
     else:
-        angle_min = angle_left
-        angle_max = angle_right
+        # Angles at left and right position of the minimum value
+        angle_left = m20[int(np.where(m20 == m20.min())[0] - 1) % 12]
+        angle_right = m20[int(np.where(m20 == m20.min())[0] + 1) % 12]
 
-    # If value at right of min is within threshold and less than the angle_right, update
-    # break loop if value surpass threshold
-    idx = int(np.where(m20 == m20.min())[0])
-    for aux in range(int(idx + 1), int(idx + 12)):
-        if m20[aux % 12] <= th:
-            if m20[aux % 12] < angle_right:
-                angle_right = m20[aux % 12]
+        # Set default minimum angle opening
+        # If both values at left and right are equal, the opening will be 60º, otherwise 30º
+        if angle_left < angle_right:
+            angle_min = angle_left
+            angle_max = m20.min()
+        elif angle_right < angle_left:
+            angle_min = m20.min()
+            angle_max = angle_right
         else:
-            break
+            angle_min = angle_left
+            angle_max = angle_right
 
-    # If value at left of min is within threshold and less than the angle_left, update
-    # break loop if value surpass threshold
-    for aux in range(idx - 1, idx - 12, -1):
-        if m20[aux % 12] <= th:
-            if m20[aux % 12] > angle_left:
-                angle_left = m20[aux % 12]
-        else:
-            break
+        # If value at right of min is within threshold and less than the angle_right, update
+        # break loop if value surpass threshold
+        idx = int(np.where(m20 == m20.min())[0])
+        for aux in range(int(idx + 1), int(idx + 12)):
+            if m20[aux % 12] <= th:
+                if m20[aux % 12] < angle_right:
+                    angle_right = m20[aux % 12]
+            else:
+                break
 
-    # Convert angle_left and angle_right to real angles:
-    angle_min = int(np.where(m20 == angle_left)[0]) * 30
-    angle_max = int(np.where(m20 == angle_right)[0]) * 30
+        # If value at left of min is within threshold and less than the angle_left, update
+        # break loop if value surpass threshold
+        for aux in range(idx - 1, idx - 12, -1):
+            if m20[aux % 12] <= th:
+                if m20[aux % 12] > angle_left:
+                    angle_left = m20[aux % 12]
+            else:
+                break
+
+        # Convert angle_left and angle_right to real angles:
+        angle_min = int(np.where(m20 == angle_left)[0]) * 30
+        angle_max = int(np.where(m20 == angle_right)[0]) * 30
 
 
 # Query full list of light pollution sources, including quarry, factory, greenhouse and shopping malls within Spain
@@ -264,45 +281,6 @@ def query_function():
     df = df.round({'lon': 4, 'lat': 4})
     df.to_csv("light_pollution_sources.csv")
 
-# Store input values received from console
-def input_values():
-    params = []
-    # Get latitude and convert into decimal format
-    latitude = input("Please enter the latitude (xxº xx' xx'' N): ").split()
-    latitude = float(latitude[0]) + (float(latitude[1]) / 60) + (float(latitude[2]) / 3600)
-
-    # Get longitude and convert into decimal format
-    longitude = input("Please enter the longitude: (xxº xx' xx'' W/E): ").split()
-    sign = longitude[3]
-    longitude = float(longitude[0]) + (float(longitude[1]) / 60) + (float(longitude[2]) / 3600)
-    if sign == "W":
-        longitude = longitude * -1
-
-    # Get distance in kilometer
-    distance = input('Please enter the radius (km): ')
-
-    # Get the central angle
-    angle = input('Please enter the central angle (º): ')
-
-    # Get the angle opening
-    opening = input('Please enter the angle opening (º): ')
-    angle_max = (float(angle) + float(opening) / 2) % 360
-    angle_min = (float(angle) - float(opening) / 2) % 360
-
-    params.append(OrderedDict({
-        'lat': latitude,
-        'lon': longitude,
-        'distance': distance,
-        'angle': angle,
-        'angle_max': angle_max,
-        'angle_min': angle_min
-    }))
-
-    global iv
-    iv = pd.DataFrame(params)
-    iv.reset_index()
-    iv = iv.astype({'lat': float, 'lon': float, 'distance': float, 'angle': float, 'angle_max': float, 'angle_min': float})
-    iv = iv.round({'lon': 4, 'lat': 4, 'distance': 2})
 
 # Filter all conditions, including distance, angle, angle opening
 def filter():
@@ -340,7 +318,6 @@ def angle_range(lon1, lat1, lon2, lat2, angle_max, angle_min):
     lat2 = math.radians(lat2)
     lon2 = math.radians(lon2)
 
-
     d_lon = lon2 - lon1
     y = math.sin(d_lon) * math.cos(lat2)
     x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(d_lon)
@@ -357,15 +334,16 @@ def angle_range(lon1, lat1, lon2, lat2, angle_max, angle_min):
     return True
 
 
+# Automatic execution
 def auto():
     read_file()
-    # input_values()
     data = filter()
 
     if not data.empty:
         print(data[["Nombre", "Tipo", "Provincia", "Poblacion", "Distancia"]])
     else:
         print('No matching records found')
+
 
 if __name__ == '__main__':
     '''
